9c8884b55d36fa92e1f814810a2ba2e432f1bf09
tufano9 <tufanox@gmail.com>
2016-04-27 12:23:36 -0430

    modificaciones varias

---
+++ app/src/main/java/com/tufano/tufanomovil/gestion/edoCuenta/gestionEdoCuenta.java
@@ -0,0 +1,18 @@
+package com.tufano.tufanomovil.gestion.edoCuenta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tufano.tufanomovil.R;
+
+/**
+ * Created por Francesco Tufano 18/4/2016.
+ */
+public class gestionEdoCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edo_cuenta);
+    }
+}
